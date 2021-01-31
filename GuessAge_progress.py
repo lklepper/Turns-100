8c42af915f8e274afad3c19aaf2ceda175c869e3
lklepper <77989060+lklepper@users.noreply.github.com>
2021-01-31 14:51:47 -0500

    Add files via upload

---
+++ GuessAge_progress.py
@@ -0,0 +1,15 @@
+'''When Will I Turn 100 Years Old?
+V1.0 by Lenishka Klepper 1/31/2020'''
+
+name = input("For starters, give me your name: ")
+print ("Ok. Well nice to meet you " + name)
+print ("........")
+print ()
+age = input ("Next, I need you to enter your age: ")
+age = int(age)
+print(".....   .......")
+print (".....damn...")
+print()
+oldYear = str((2021 - age)+100)
+print("Alright it looks like you will be 100 Years Old in the Year: " + (oldYear))
+      
